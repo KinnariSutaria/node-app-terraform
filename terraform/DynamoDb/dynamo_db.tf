c8713b5d7064f4d99a3465bbbdcbf78d1aa80cd7
kinnarisutaria <kinnarisutaria9@gmail.com>
2023-06-22 12:45:23 +0530

    Terraform code

--- terraform/DynamoDb/dynamo_db.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "demo_table" {
-  name           = "AWSWorkshopDemoTable"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "PK"
-  attribute {
-    name = "PK"
-    type = "S"
-  }
-}
---
+++ terraform/lambda.tf
@@ -0,0 +1,53 @@
+resource "aws_lambda_function" "create_short_url" {
+  function_name = "create_short_url"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  runtime       = "nodejs14.x"
+  handler       = "index.handler"
+  filename      = "../createShortURL.js.zip"
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_lambda_function" "get_long_url" {
+  function_name = "get_long_url"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  runtime       = "nodejs14.x"
+  handler       = "index.handler"
+  filename      = "../getLongURL.js.zip"
+  role = aws_iam_role.lambda_exec.arn
+}
+
+
+resource "aws_lambda_layer_version" "node_layer" {
+  filename   = "my_layer.zip"
+  layer_name = "node_layer"
+  compatible_runtimes = ["nodejs14.x"]
+  source_code_hash = filebase64sha256("my_layer.zip")
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_example_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
