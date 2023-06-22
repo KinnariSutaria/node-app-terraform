68c97e59af7ea05ce11de028d9fb531308dcc50d
kinnarisutaria <kinnarisutaria9@gmail.com>
2023-06-22 23:23:31 +0530

    Terraform cleanup code

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "demo_table" {
+resource "aws_dynamodb_table" "AWSWorkshopDemoTable" {
   name           = "AWSWorkshopDemoTable"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "PK"
@@ -7,3 +7,27 @@ resource "aws_dynamodb_table" "demo_table" {
     type = "S"
   }
 }
+
+resource "aws_iam_policy" "dynamoDBLambdaPolicy" {
+  name = "DynamoDBLambdaPolicy"
+
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "dynamodb:*"
+          ],
+          "Resource": [
+            "arn:aws:dynamodb:*:*:table/*"
+          ]
+        }
+      ]
+    }
+
+
+  )
+}
+
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,8 +6,8 @@ resource "aws_lambda_function" "create_short_url" {
   # exported in that file.
   runtime       = "nodejs14.x"
   handler       = "index.handler"
-  filename      = "../createShortURL.js.zip"
-  role = aws_iam_role.lambda_exec.arn
+  filename      = "createShortURL.js.zip"
+  role = aws_iam_role.lambdaRole.arn
   layers = [aws_lambda_layer_version.node_layer.arn]
 }
 
@@ -19,14 +19,14 @@ resource "aws_lambda_function" "get_long_url" {
   # exported in that file.
   runtime       = "nodejs14.x"
   handler       = "index.handler"
-  filename      = "../getLongURL.js.zip"
-  role = aws_iam_role.lambda_exec.arn
+  filename      = "getLongURL.js.zip"
+  role = aws_iam_role.lambdaRole.arn
   layers = [aws_lambda_layer_version.node_layer.arn]
 }
 
 
 resource "aws_lambda_layer_version" "node_layer" {
-  filename   = "../lambdalayer.zip"
+  filename   = "lambdalayer.zip"
   layer_name = "node_layer"
   compatible_runtimes = ["nodejs14.x"]
 
@@ -34,22 +34,73 @@ resource "aws_lambda_layer_version" "node_layer" {
 
 # IAM role which dictates what other AWS services the Lambda function
 # may access.
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_example_lambda"
+resource "aws_iam_role" "lambdaRole" {
+  name = "LambdaRole"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
-EOF
+
+resource "aws_iam_role_policy_attachment" "lambda-policy-attachment" {
+  role       = aws_iam_role.lambdaRole.name
+  policy_arn = aws_iam_policy.dynamoDBLambdaPolicy.arn
+}
+
+resource "aws_iam_policy" "cloudWatchLambdaPolicy" {
+  name = "cloudWatchLambdaPolicy"
+
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "autoscaling:Describe*",
+            "cloudwatch:*",
+            "logs:*",
+            "sns:*",
+            "iam:GetPolicy",
+            "iam:GetPolicyVersion",
+            "iam:GetRole",
+            "oam:ListSinks"
+          ],
+          "Resource": "*"
+        },
+        {
+          "Effect": "Allow",
+          "Action": "iam:CreateServiceLinkedRole",
+          "Resource": "arn:aws:iam::*:role/aws-service-role/events.amazonaws.com/AWSServiceRoleForCloudWatchEvents*",
+          "Condition": {
+            "StringLike": {
+              "iam:AWSServiceName": "events.amazonaws.com"
+            }
+          }
+        },
+        {
+          "Effect": "Allow",
+          "Action": [
+            "oam:ListAttachedLinks"
+          ],
+          "Resource": "arn:aws:oam:*:*:sink/*"
+        }
+      ]
+    }
+
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch-policy-attachment" {
+  role       = aws_iam_role.lambdaRole.name
+  policy_arn = aws_iam_policy.cloudWatchLambdaPolicy.arn
 }
